89c6b051de5641eb0308ae394daf9b822a666d44
Oleksiy Nosov <oleksiynosov@gmail.com>
2017-04-19 22:08:06 +0300

    Initialize project

---
+++ WebApp/Controllers/ShowController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class ShowController : Controller
+    {
+        // GET: Show
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ShowSomething(int id)
+        {
+            return Content($"You send me to id {id}");
+        }
+
+        [HttpPost]
+        public ActionResult PostForm(PostData item)
+        {
+            return Content($"Hello {item.FirstName} {item.LastName},Your Age: {item.Age} \nGender: {item.Gender}");
+        }
+    }
+}
---
+++ WebApp/Models/PostData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class PostData
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Field cannot be empty")]
+        [MaxLength(350, ErrorMessage = "Field must contain less characters")]
+        public string FirstName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Field cannot be empty")]
+        [MaxLength(350, ErrorMessage = "Field must contain less characters")]
+        public string LastName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Field cannot be empty")]
+        [MaxLength(350, ErrorMessage = "Field must contain less characters")]
+        [MinLength(3, ErrorMessage = "Field must contain more than 2 characters")]
+        public string Login { get; set; }
+
+        [Range(18, 140)]
+        public string Age { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Email { get; set; }
+    }
+}
